9debd88d7852b720abfaa6d003a5b8dc678be1ae
kimzapp <giaphk2012004@gmail.com>
2026-06-24 14:51:27 +0700

    fix migrate artifacts

--- migrate_artifacts/download_artifacts.sh
+++ migrate_artifacts/download_artifacts.sh
@@ -8,19 +8,24 @@ curl -L -o ./bge-small-en-v15-embedding-faiss.zip https://www.kaggle.com/api/v1/
 curl -L -o ./dense-index-vi.zip https://www.kaggle.com/api/v1/datasets/download/hongkimgip/dense-index-vi
 
 # unzip all files
-unzip bge-small-en-v15-embedding-faiss.zip
-unzip bm25-index.zip
-unzip bm25-index-vi.zip
-unzip cache-artifacts.zip
-unzip dense-index-vi.zip
-unzip paraphrased-query.zip
-unzip runs-artifacts.zip
-
-# remove zip files
+unzip bge-small-en-v15-embedding-faiss.zip -d bge_small_en_v1.5_embedding_faiss
 rm bge-small-en-v15-embedding-faiss.zip
+
+unzip bm25-index.zip -d bm25_index
 rm bm25-index.zip
+
+unzip bm25-index-vi.zip -d bm25_index_vi
 rm bm25-index-vi.zip
+
+unzip cache-artifacts.zip -d cache
 rm cache-artifacts.zip
+
+unzip dense-index-vi.zip -d dense_index_vi
 rm dense-index-vi.zip
+
+unzip paraphrased-query.zip -d paraphrased_query
 rm paraphrased-query.zip
+
+unzip runs-artifacts.zip -d runs
+rm runs-artifacts.zip
 rm runs-artifacts.zip
